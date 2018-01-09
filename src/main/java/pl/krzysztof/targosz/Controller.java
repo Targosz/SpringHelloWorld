75a8e42e442f086c666d8c49afb5c0709c87f2e6
montezuma <zibidezeb.t@gmail.com>
2018-01-09 20:29:24 +0100

    First commit

---
+++ src/main/java/pl/krzysztof/targosz/Controller.java
@@ -0,0 +1,13 @@
+package pl.krzysztof.targosz;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Hello World!";
+    }
+}
